816410d0f149431434a8c34ee8e73c62c19c0413
Mikus Kukainis <mikuskukainis@gmail.com>
2023-09-05 21:59:31 +0300

    Vue Reaction Game

---
+++ src/components/Results.vue
@@ -0,0 +1,32 @@
+<template>
+  <p>Reaction time: {{ score }} ms</p>
+  <p class="rank">{{ rank }}</p>
+</template>
+
+<script>
+export default {
+    props: ['score'],
+    data() {
+        return {
+            rank: null
+        }
+    },
+    mounted() {
+        if (this.score < 250) {
+            this.rank = 'Turbo fingers'
+        } else if (this.score < 400) {
+            this.rank = 'Not bad'
+        } else {
+            this.rank = 'Snail...'
+        }
+    }
+}
+</script>
+
+<style>
+    .rank {
+        font-size: 1.4em;
+        color: #0faf87;
+        font-weight: bold;
+    }
+</style>
